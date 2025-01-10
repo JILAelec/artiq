8ba58b28ef840878ea60d3bc485cac9e655a0aa1
Felix Vietmeyer <Felix.Vietmeyer@jila.colorado.edu>
2025-01-10 18:01:17 -0500

    fix mistake

--- artiq/gateware/targets/efc.py
+++ artiq/gateware/targets/efc.py
@@ -271,10 +271,10 @@ class Satellite(BaseSoC, AMPSoC):
             self.rtio_channels.append(rtio.Channel.from_phy(ltc2000_spi_phy))
 
             # LTC2000 and its DDS submodule
-            ltc2000_dds = LTC2000(self.platform, ltc2000_pads)
+            self.ltc2000_dds = LTC2000(self.platform, ltc2000_pads)
             self.submodules += ltc2000_dds
             print("LTC2000 DAC at RTIO channel 0x{:06x}".format(len(self.rtio_channels)))
-            self.rtio_channels.extend(rtio.Channel.from_phy(phy) for phy in ltc2000_dds.phys)
+            self.rtio_channels.extend(rtio.Channel.from_phy(phy) for phy in self.ltc2000_dds.phys)
 
             self.clock_domains.cd_sys2x = ClockDomain(reset_less=True)
             self.clock_domains.cd_sys6x = ClockDomain(reset_less=True)
